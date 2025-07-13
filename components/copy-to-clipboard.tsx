3143672e9d0e46fc06b295835b3864d1a69d7fa5
toby <contact@bundui.com>
2025-07-13 19:41:02 +0300

    build error

--- components/copy-to-clipboard.tsx
+++ components/copy-to-clipboard.tsx
@@ -1,21 +1,29 @@
 "use client";
 
-import { CopyToClipboard as CopyToClipboardComp } from "react-copy-to-clipboard";
-import { Check, Clipboard } from "lucide-react";
 import { useState } from "react";
+import { Check, Clipboard } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default function CopyToClipboard({ text }: { text: string }) {
   const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1000);
+    } catch (err) {
+      console.error("Kopyalama başarısız:", err);
+    }
+  };
+
   return (
-    <CopyToClipboardComp text={text} onCopy={() => setTimeout(() => setCopied(false), 1000)}>
-      <Button className="bg-transparent!" onClick={() => setCopied(true)}>
-        {copied ? (
-          <Check className="h-4 w-4 text-white/50" />
-        ) : (
-          <Clipboard className="h-4 w-4 text-white/50" />
-        )}
-      </Button>
-    </CopyToClipboardComp>
+    <Button className="bg-transparent!" onClick={handleCopy}>
+      {copied ? (
+        <Check className="h-4 w-4 text-white/50" />
+      ) : (
+        <Clipboard className="h-4 w-4 text-white/50" />
+      )}
+    </Button>
   );
 }
