1763e0d60c491ab94e7346850f05784b6535e13d
toby <contact@bundui.com>
2025-07-22 11:21:38 +0300

    ga

--- lib/ga.ts
+++ lib/ga.ts
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 export default function GoogleAnalyticsInit() {
   useEffect(() => {
-    ReactGA.initialize(`${process.env.GA_KEY}`);
+    ReactGA.initialize(`${process.env.NEXT_PUBLIC_GA_KEY}`);
     ReactGA.send("pageview");
   });
 
