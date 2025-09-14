14588f39b2e49ff54be31914bd3080fed1901152
toby <hello@tobybelhome.com>
2025-09-14 23:11:58 +0300

    image opt

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "shadcnexamples"
+        hostname: "shadcnexamples.com"
       },
       {
         protocol: "https",
